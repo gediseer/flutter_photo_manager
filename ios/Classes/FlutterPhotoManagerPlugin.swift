6bfaf63169d0407e7d881d969c931fad22a741b2
mao <xueyingmao@outlook.com>
2024-05-01 13:28:46 +0800

    fix photo manager plugin duplicate

--- ios/Classes/FlutterPhotoManagerPlugin.swift
+++ ios/Classes/FlutterPhotoManagerPlugin.swift
@@ -1,19 +1,19 @@
-import Flutter
-import UIKit
-
-public class FlutterPhotoManagerPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_photo_manager", binaryMessenger: registrar.messenger())
-    let instance = FlutterPhotoManagerPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
-    }
-  }
-}
+// import Flutter
+// import UIKit
+//
+// public class FlutterPhotoManagerPlugin: NSObject, FlutterPlugin {
+//   public static func register(with registrar: FlutterPluginRegistrar) {
+//     let channel = FlutterMethodChannel(name: "flutter_photo_manager", binaryMessenger: registrar.messenger())
+//     let instance = FlutterPhotoManagerPlugin()
+//     registrar.addMethodCallDelegate(instance, channel: channel)
+//   }
+//
+//   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+//     switch call.method {
+//     case "getPlatformVersion":
+//       result("iOS " + UIDevice.current.systemVersion)
+//     default:
+//       result(FlutterMethodNotImplemented)
+//     }
+//   }
+// }
